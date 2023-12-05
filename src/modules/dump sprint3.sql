8647b6c3c44fc9fabc504485cb304e0595c2898d
Leon <leonamorim.l93@gmail.com>
2023-12-04 23:06:52 -0300

    corrigindo dump

--- src/modules/dump sprint3.sql
+++ src/modules/dump sprint3.sql
@@ -3,15 +3,15 @@ alter table produtos add column produto_imagem text;
 
 create table pedidos(
     id serial primary key,
-    cliente_id references clientes(id) not null,
+    cliente_id int references clientes(id) not null,
     observacao varchar(100),
     valor_total int not null
 );
 
 create table pedido_produtos(
     id serial primary key,
-    pedido_id references pedidos(id),
-    produto_id references produtos(id),
+    pedido_id int references pedidos(id),
+    produto_id int references produtos(id),
     quantidade_produto int not null,
     valor_produto int not null
 );
